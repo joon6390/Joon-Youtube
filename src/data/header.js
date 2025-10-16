86d28e9e00286204efb0ca1eeab088ef574e2555
joon6390 <haejunbag131@google.com>
2025-10-16 22:55:50 +0900

    헤더 영역 데이터 작업 완료

---
+++ src/data/header.js
@@ -0,0 +1,112 @@
+import { CiBaseball } from "react-icons/ci";
+import { CiCoins1 } from "react-icons/ci";
+import { CiBoxes } from "react-icons/ci";
+import { CiBullhorn } from "react-icons/ci";
+import { CiCoffeeCup } from "react-icons/ci";
+import { CiDumbbell } from "react-icons/ci";
+import { CiFries } from "react-icons/ci";
+import { CiMoneyBill } from "react-icons/ci";
+
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+export const headerMenus = [
+    {
+        title: "Joon",
+        icon: <CiBaseball />,
+        src: "/"
+    },
+    {
+        title: "추천 영상",
+        icon: <CiMoneyBill />,
+        src: "/today"
+    },
+    {
+        title: "추천 개발자",
+        icon: <CiCoins1 />,
+        src: "/developer"
+    },
+    {
+        title: "웹 디자인 개발 기능사",
+        icon: <CiBoxes />,
+        src: "/webd"
+    },
+    {
+        title: "웹 표준 사이트",
+        icon: <CiBullhorn />,
+        src: "/website"
+    },
+    {
+        title: "GSAP Parallax",
+        icon: <CiCoffeeCup />,
+        src: "/gsap"
+    },
+    {
+        title: "포트폴리오 사이트",
+        icon: <CiDumbbell />,
+        src: "/port"
+    },
+    {
+        title: "나만의 유튜브 사이트",
+        icon: <CiFries />,
+        src: "/youtube"
+    },
+];
+
+export const searchKeyword = [
+    {
+        title: "HTML",
+        src: "/search/html"
+    },
+    {
+        title: "CSS",
+        src: "/search/css"
+    },
+    {
+        title: "JavaScript",
+        src: "/search/javascript"
+    },
+    {
+        title: "React.js",
+        src: "/search/react.js"
+    },
+    {
+        title: "Next.js",
+        src: "/search/next.js"
+    },
+    {
+        title: "Node.js",
+        src: "/search/node.js"
+    },
+    {
+        title: "SQL",
+        src: "/search/sql"
+    },
+    {
+        title: "portfolio",
+        src: "/search/React Portfolio"
+    },
+    {
+        title: "music",
+        src: "/search/NewJeans"
+    }
+];
+
+export const snsLink = [
+    {
+        title: "github",
+        url: "https://github.com/joon6390",
+        icon: <AiFillGithub />
+    },
+    {
+        title: "youtube",
+        url: "https://www.youtube.com/joon6390",
+        icon: <AiFillYoutube />
+    },
+    {
+        title: "instagram",
+        url: "https://www.instagram.com/pxhxjxn",
+        icon: <AiOutlineInstagram />
+    },
+]
