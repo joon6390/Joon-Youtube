16e84bacaabbe0b2111bde6c4c07e32822db5ebc
joon6390 <joon6390@naver.com>
2025-10-16 23:47:14 +0900

    페이지 SEO 작업 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import Main from '../components/section/Main'
 
 const Home = () => {
     return (
-        <Main>
+        <Main title="Joon 유튜브" 
+        description="Joon 유튜브에 오신 것을 환영합니다."
+        >
             Home
         </Main>
     )
--- src/pages/Youtube.jsx
+++ src/pages/Youtube.jsx
@@ -3,7 +3,9 @@ import Main from '../components/section/Main'
 
 const Youtube = () => {
     return (
-        <Main>
+        <Main 
+            title = "나만의 유튜브 사이트"
+            description="나만의 유튜브 사이트 튜토리얼 강의입니다.">
             Youtube
         </Main>
     )
