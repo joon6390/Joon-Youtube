38e573b5387e9f4f1146609312e8dc1a0f78d301
joon6390 <haejunbag131@google.com>
2025-10-17 23:34:53 +0900

    메인 섹션 나머지 컨텐츠 작업 완료

--- src/components/contents/Gsap.jsx
+++ src/components/contents/Gsap.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { gsapText } from '../../data/gsap'
+import { Link } from 'react-router-dom'
+
 const Gsap = () => {
-  return (
-    <div>Gsap</div>
-  )
+    return (
+        <section id='gsap'>
+            <h2>GSAP 패럴랙스 효과 연습</h2>
+            <div className="video__inner">
+                {gsapText.map((video, key) => (
+                    <div className="video" key={key}>
+                        <div className="video__thumb play__icon">
+                            <Link to={`/video/${video.videoId}`}>
+                                <img src={video.img} alt={video.title} />
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default Gsap
