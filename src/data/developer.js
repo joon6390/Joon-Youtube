b37fc1c01262b69865bff59f4a3d0516481cacfb
joon6390 <joon6390@naver.com>
2025-10-17 22:51:55 +0900

    추천 개발자 작업 완료

---
+++ src/data/developer.js
@@ -0,0 +1,116 @@
+export const developerText = [
+    {
+        img: "https://yt3.googleusercontent.com/6gJoxwN9IQ3SlQIo1SibzhmwNLfxhX3t15zfQAGMxmV3Fi7aALzNayf2CzqhL5U6c2f4123G=s900-c-k-c0x00ffffff-no-rj",
+        name: "조코딩 JoCoding",
+        channelAddress: "https://www.youtube.com/@jocoding",
+        channelId: "UCQNE2JmbasNYbjGAcuBiRRg"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaSNRWU5nI-gDyzEdtbhpLqWg8wXNs-FQHf35iMCOw=s176-c-k-c0x00ffffff-no-rj",
+        name: "생활코딩",
+        channelAddress: "https://www.youtube.com/@coohde",
+        channelId: "UCvc8kv-i5fvFTJBFAk6n1SA"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaSn88PvVXWN_Wy5vSr-5Eo_NGDdw_apL6bM7QKfCw=s176-c-k-c0x00ffffff-no-rj",
+        name: "노마드 코더 Nomad Coders",
+        channelAddress: "https://www.youtube.com/@nomadcoders",
+        channelId: "UCUpJs89fSBXNolQGOYKn0YQ"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaSeGpiSPyozdunhf74J4oa2XWHPObm8bKTcbEU=s176-c-k-c0x00ffffff-no-rj",
+        name: "코딩하는거니",
+        channelAddress: "https://www.youtube.com/@gunnycoding",
+        channelId: "UCO7g158NWgLyn98z8v3zduA"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/Jvq19f8uwgW2hYu5-Y5QR8W9r71cwdgyXaskvdEDm78iVCZZQPP7tsvQsYtWrwZ7XULX9G7-6w=s900-c-k-c0x00ffffff-no-rj",
+        name: "워니코딩",
+        channelAddress: "https://www.youtube.com/@WonieSong",
+        channelId: "UC0uDM1xZMNBAoW2xnzhAQ7g"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaRCkXnBnWEMfpo5c_TXB-tF0FxqiCfv5KoP_9IBC6A=s176-c-k-c0x00ffffff-no-rj",
+        name: "얄팍한 코딩사전",
+        channelAddress: "https://www.youtube.com/@yalco-coding",
+        channelId: "UC2nkWbaJt1KQDi2r2XclzTQ"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/-uLq1lI1er_M8AM8ghMZnv6djtfif2kog6aOqXnO4D6SQpaI3oMJmb_OJwKsTVqwgiYnVEPPOs8=s900-c-k-c0x00ffffff-no-rj",
+        name: "개발하는 정대리",
+        channelAddress: "https://www.youtube.com/@dev_jeongdaeri",
+        channelId: "UCutO2H_AVmWHbzvE92rpxjA"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaTw2ZRRi6UzhNeunJh03HOeAnKScgRJygpN7_MsVQ=s176-c-k-c0x00ffffff-no-rj",
+        name: "코딩애플",
+        channelAddress: "https://www.youtube.com/@codingapple",
+        channelId: "UCSLrpBAzr-ROVGHQ5EmxnUg"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaRfTsQsimyyMrWugbbogh_EdsCWWQsKVqh5Fdvq=s176-c-k-c0x00ffffff-no-rj",
+        name: "김왼손의 왼손코딩",
+        channelAddress: "https://www.youtube.com/@leftykhim",
+        channelId: "UC0h8NzL2vllvp3PjdoYSK4g"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaSXgyXVlJGDyr1IaQgHJlGvc4wcFcZJ4S35NATdrYA=s176-c-k-c0x00ffffff-no-rj",
+        name: "제주코딩베이스캠프",
+        channelAddress: "https://www.youtube.com/@jejucodingcamp",
+        channelId: "UC4GnvNKtuJ4cqWsYjxNxAEQ"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaQyV-QwNnxxLKCnfHZrfsGNx7fqFD9YSQ_lweIx6w=s176-c-k-c0x00ffffff-no-rj",
+        name: "나도코딩",
+        channelAddress: "https://www.youtube.com/@nadocoding",
+        channelId: "UC7iAOLiALt2rtMVAWWl4pnw"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaTiHU0ol325VdijS5AX4DQomGs6zlD_y_v0I0A6=s176-c-k-c0x00ffffff-no-rj",
+        name: "라매개발자",
+        channelAddress: "https://www.youtube.com/@lamedev",
+        channelId: "UCr3lkp7TJiJTCY4ffRQ1m2A"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaSnppVkSh38WlQrn-7FzBAqlJpMLdGq4jS2GGHzgQ=s176-c-k-c0x00ffffff-no-rj",
+        name: "드림코딩",
+        channelAddress: "https://www.youtube.com/@dream-coding",
+        channelId: "UC_4u-bXaba7yrRz_6x6kb_w"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaTyIgb-N-SeFznHEn_RkJkF9IirgzvXtEoWOnNt=s176-c-k-c0x00ffffff-no-rj",
+        name: "ZeroCho TV",
+        channelAddress: "https://www.youtube.com/@ZeroChoTV",
+        channelId: "UCp-vBtwvBmDiGqjvLjChaJw"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/QS63zfFuCmQZh0NjNZ5JKdgCRgRMZDkdsVZj_Tn3ialcewCOqY4HDcmHt5utqTHfMqzZsG-o0w=s900-c-k-c0x00ffffff-no-rj",
+        name: "짐코딩 GYM CODING",
+        channelAddress: "https://www.youtube.com/@gymcoding",
+        channelId: "UCZ30aWiMw5C8mGcESlAGQbA"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/DSggIhy54EmSQdEuLYgJE0pcnGzFUuOMSa-o-7MqPNk4Wv5ox95soWHaCHjYmhvQ-Ns9P7gLxw=s900-c-k-c0x00ffffff-no-rj",
+        name: "프롱트",
+        channelAddress: "https://www.youtube.com/@frongt",
+        channelId: "UCku8mOp8y05Cm6sb9Rp0jMQ"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/TAd4mSVptqEaIrFw3zVrOHXIQTHw5jROLwivGD_THhPeZST97J0b4duFjnL_dBNWYsDZgANsMg=s900-c-k-c0x00ffffff-no-rj",
+        name: "프로그래머 김플 스튜디오",
+        channelAddress: "https://www.youtube.com/@kimfl",
+        channelId: "UCdNSo3yB5-FRTFGbUNKNnwQ"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaQneLC0nfXjK4QaNPSG1lZXCUZtuhoFdBtYz1hH=s176-c-k-c0x00ffffff-no-rj",
+        name: "코딩알려주는누나",
+        channelAddress: "https://www.youtube.com/@user-yu8so2ck1z",
+        channelId: "UCfBvs0ZJdTA43NQrnI9imGA"
+    },
+    {
+        img: "https://yt3.googleusercontent.com/ytc/AOPolaQxJlRUxIrCv4zw_kfUsqKHAm1rN32SHAU8Iwla=s176-c-k-c0x00ffffff-no-rj",
+        name: "동빈나",
+        channelAddress: "https://www.youtube.com/@dongbinna",
+        channelId: "UChflhu32f5EUHlY7_SetNWw"
+    },
+]
